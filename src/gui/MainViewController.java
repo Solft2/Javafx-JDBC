06e40afa29fcc3bd6e389f23fdc4a6f9acbd3792
Solft2 <arthureric33@gmail.com>
2023-06-02 19:26:30 -0300

    DepartmentList view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -29,7 +29,7 @@ public class MainViewController implements Initializable {
     }
 	@FXML
 	public void onMenuItemDepartmentAction() {
-    	System.out.println("Menu Department clicado");
+		loadView("/gui/DepartmentList.fxml");
     }
 	@FXML
 	public void onMenuItemaboutAction() {
