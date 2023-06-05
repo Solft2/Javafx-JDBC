55fe468f021928002bbaa4288980f6891df7b010
Solft2 <arthureric33@gmail.com>
2023-06-05 17:21:07 -0300

    SellerList Tableview

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable {
 	@FXML
@@ -27,7 +28,10 @@ public class MainViewController implements Initializable {
 	private MenuItem about; 
 	@FXML
     public void onMenuItemSellerAction() {
-    	System.out.println("Menu seller clicado");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
     }
 	@FXML
 	public void onMenuItemDepartmentAction() {
